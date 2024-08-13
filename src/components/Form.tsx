ef8d16ffbb28d55a5165cea209b07744c3ffa3d5
rishijain2002 <rishijain0652@gmail.com>
2024-08-13 14:34:52 +0530

    Submit From Data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,30 @@
-
+import {useForm} from 'react-hook-form';
 
 export const Form = () => {
+
+  type FormValues={
+    name:string,
+    email:string,
+    password:string
+  }
+
+  const form =useForm<FormValues>();
+  const {register,handleSubmit}=form;
+
+  const onSubmit=(data:FormValues)=>{
+    console.log("submit",data);
+  }
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" placeholder="Name" />
+            <input type="text" id='name' placeholder="Name" {...register("name")} />
 
-            <label>Email</label>
-            <input type="email" name="email" placeholder="Email" />
+            <label htmlFor="email" >Email</label>
+            <input type="email"  placeholder="Email" {...register("email")} />
 
-            <label>Password</label>
-            <input type="password" name="password" placeholder="Password" />
+            <label htmlFor="password" >Password</label>
+            <input type="password"  placeholder="Password" {...register("password")} />
 
             <label>Confirm Password</label>
             <input type="password" name="Confirm-password" placeholder="Confirm Password" />
