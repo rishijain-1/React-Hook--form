960b27e993c911ea61fe21d27138b6da174ddd11
rishijain2002 <rishijain0652@gmail.com>
2024-08-13 15:33:26 +0530

    Add confirm Password functionality

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,15 @@
-import { useForm, } from "react-hook-form";
-import type {FieldValues} from "react-hook-form"
+import { useForm } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
 
 export const Form = () => {
-
-
   const form = useForm<FieldValues>();
-  const { register, handleSubmit, formState:{errors,isSubmitting},reset } = form;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = form;
 
   const onSubmit = (data: FieldValues) => {
     console.log("submit", data);
@@ -15,25 +19,37 @@ export const Form = () => {
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" placeholder="Name" {...register("name",{required:"Name is required"})} />
-        {errors.name &&(
+        <input
+          type="text"
+          id="name"
+          placeholder="Name"
+          {...register("name", { required: "Name is required" })}
+        />
+        {errors.name && (
           <p className="form-error-msg">{`${errors.name.message}`}</p>
         )}
 
         <label htmlFor="email">Email</label>
-        <input type="email" placeholder="Email" {...register("email",{required:"Email is required"})} />
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: "Email is required" })}
+        />
         {errors.email && (
           <p className="form-error-msg">{`${errors.email.message}`}</p>
-        )} 
+        )}
 
         <label htmlFor="password">Password</label>
         <input
           type="password"
           placeholder="Password"
-          {...register("password",{required:"Password is required", minLength:{
-            value:6,
-            message:"Password should be atleast 6 characters"
-          }})}
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password should be atleast 6 characters",
+            },
+          })}
         />
 
         {errors.password && (
@@ -44,10 +60,20 @@ export const Form = () => {
         <input
           type="password"
           placeholder="Confirm-Password"
-          {...register("confirmPassword",{required:"Confirm Password is required", validate: (value : string) => value === form.getValues("password") || "Password does not match"})}
+          {...register("confirmPassword", {
+            required: "Confirm Password is required",
+            validate: (value: string) =>
+              value === getValues("password") || "Password does not match",
+          })}
         />
 
-        <button disabled={isSubmitting} type="submit">Submit</button>
+        {errors.confirmPassword && (
+          <p className="form-error-msg">{`${errors.confirmPassword.message}`}</p>
+        )}
+
+        <button disabled={isSubmitting} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
