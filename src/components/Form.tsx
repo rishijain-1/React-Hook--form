9f4c4c3b3283b003e117d691802338907b87228f
rishijain2002 <rishijain0652@gmail.com>
2024-08-13 15:03:55 +0530

    Add validation in fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,47 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 export const Form = () => {
+  type FormValues = {
+    name: string;
+    email: string;
+    password: string;
+  };
 
-  type FormValues={
-    name:string,
-    email:string,
-    password:string
-  }
+  const form = useForm<FormValues>();
+  const { register, handleSubmit, formState:{errors,isSubmitting},reset } = form;
 
-  const form =useForm<FormValues>();
-  const {register,handleSubmit}=form;
-
-  const onSubmit=(data:FormValues)=>{
-    console.log("submit",data);
-  }
+  const onSubmit = (data: FormValues) => {
+    console.log("submit", data);
+    reset();
+  };
   return (
     <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">Name</label>
-            <input type="text" id='name' placeholder="Name" {...register("name")} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" placeholder="Name" {...register("name",{required:"Name is required"})} />
 
-            <label htmlFor="email" >Email</label>
-            <input type="email"  placeholder="Email" {...register("email")} />
+        <label htmlFor="email">Email</label>
+        <input type="email" placeholder="Email" {...register("email",{required:"Email is required"})} />
 
-            <label htmlFor="password" >Password</label>
-            <input type="password"  placeholder="Password" {...register("password")} />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password",{required:"Password is required", minLength:{
+            value:6,
+            message:"Password should be atleast 6 characters"
+          }})}
+        />
 
-            <label>Confirm Password</label>
-            <input type="password" name="Confirm-password" placeholder="Confirm Password" />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          name="Confirm-password"
+          placeholder="Confirm Password"
+        />
 
-            <button type="submit">Submit</button>
-        </form>
+        <button disabled={isSubmitting} type="submit">Submit</button>
+      </form>
     </div>
   );
 };
