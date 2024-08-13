6a6b3bc8c0be7ef98ccea664a8648353fe0c25da
rishijain2002 <rishijain0652@gmail.com>
2024-08-13 15:23:55 +0530

    Error handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,13 @@
-import { useForm } from "react-hook-form";
+import { useForm, } from "react-hook-form";
+import type {FieldValues} from "react-hook-form"
 
 export const Form = () => {
-  type FormValues = {
-    name: string;
-    email: string;
-    password: string;
-  };
 
-  const form = useForm<FormValues>();
+
+  const form = useForm<FieldValues>();
   const { register, handleSubmit, formState:{errors,isSubmitting},reset } = form;
 
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = (data: FieldValues) => {
     console.log("submit", data);
     reset();
   };
@@ -19,9 +16,15 @@ export const Form = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name</label>
         <input type="text" id="name" placeholder="Name" {...register("name",{required:"Name is required"})} />
+        {errors.name &&(
+          <p className="form-error-msg">{`${errors.name.message}`}</p>
+        )}
 
         <label htmlFor="email">Email</label>
         <input type="email" placeholder="Email" {...register("email",{required:"Email is required"})} />
+        {errors.email && (
+          <p className="form-error-msg">{`${errors.email.message}`}</p>
+        )} 
 
         <label htmlFor="password">Password</label>
         <input
@@ -33,11 +36,15 @@ export const Form = () => {
           }})}
         />
 
+        {errors.password && (
+          <p className="form-error-msg">{`${errors.password.message}`}</p>
+        )}
+
         <label>Confirm Password</label>
         <input
           type="password"
-          name="Confirm-password"
-          placeholder="Confirm Password"
+          placeholder="Confirm-Password"
+          {...register("confirmPassword",{required:"Confirm Password is required", validate: (value : string) => value === form.getValues("password") || "Password does not match"})}
         />
 
         <button disabled={isSubmitting} type="submit">Submit</button>
